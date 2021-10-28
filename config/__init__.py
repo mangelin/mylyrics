86261ebfad2e6abe2d8e0cbc52c8c19c97e35d72
Matteo Angelino <matteo.angelino@gmail.com>
2021-10-28 15:20:08 +0200

    config: add providers method to list all registerd providers

--- config/__init__.py
+++ config/__init__.py
@@ -6,6 +6,12 @@ load_dotenv()
 AZLYRICS_PROXY="AZLyrics"
 ELYRICS_PROXY="ELyrics"
 
+
+__PROVIDERS = [
+    AZLYRICS_PROXY,
+    ELYRICS_PROXY
+]
+
 AZLYRICS_SEARCH_URL = os.getenv("AZLYRICS_SEARCH_URL")
 
 ELYRICS_BASE_URL = os.getenv("ELYRICS_BASE_URL")
@@ -14,3 +20,6 @@ ELYRICS_SEARCH_URL = f"{ELYRICS_BASE_URL}/find.php"
 OUTPUT_FORMAT = os.getenv("DEFAULT_OUTPUT_FORMAT")
 
 DEFAULT_SAVE_DIRECTORY = os.getenv("DEFAULT_SAVE_DIRECTORY", os.curdir)
+
+def providers():
+    return list(map(lambda p:p.lower(),__PROVIDERS))
