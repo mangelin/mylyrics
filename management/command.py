7da27470006fe11d0d17cf960c14c067e5b2d323
Matteo Angelino <matteo.angelino@gmail.com>
2021-10-28 10:59:16 +0200

    command: refactoring exception management

--- management/command.py
+++ management/command.py
@@ -16,14 +16,17 @@ def save_to_folder(artist:str, song_name:str, lyrics:str):
     try:
         p = Path(artist_directory_name)
         p.mkdir(parents=True)
+
+        file_name = os.path.join(artist_directory_name, f"{slugify(song_name)}.txt")
+
+        with open(file_name,"w+") as f:
+            f.write(lyrics)
+    except PermissionError as ep:
+        sys.exit(f"Error on save: {ep}\n")
     except Exception as e:
-        print(e)
+        sys.exit(f"Unexpected Error on save: {e}\n")
 
-    file_name = os.path.join(artist_directory_name, f"{slugify(song_name)}.txt")
-
-    with open(file_name,"w+") as f:
-        f.write(lyrics)
-
+    
 def load_from_folder(artist:str, song_name:str):
     artist_directory_name = f"{DEFAULT_SAVE_DIRECTORY}/{slugify(artist)}"
     file_name = os.path.join(artist_directory_name, f"{slugify(song_name)}.txt")
@@ -32,8 +35,10 @@ def load_from_folder(artist:str, song_name:str):
     try:
         with open(file_name, 'r') as f:
             data = f.read()
-    except Exception as e:
-        print(e)
+    except FileNotFoundError as e:
+        pass
+    except Exception as e1:
+        sys.exit(f"Warning: {e1}\n")
     
     return data
 
@@ -63,18 +68,22 @@ class MyLyricsCommand(object):
         lyrics = load_from_folder(args.artist, args.lyrics)
         if lyrics:
             sys.stdout.write(lyrics)
-            sys.exit(0)
+            return
 
         
         # Try to fetch lyrics by provider
         provider = self.factory.create_proxy(args.provider)
-        lyrics = provider.get_lyrics(args.artist, args.lyrics)
+
+        try:
+            lyrics = provider.get_lyrics(args.artist, args.lyrics)
+        except ValueError as e:
+            sys.exit(f"{e}\n")
     
         if not lyrics:
             sys.stdout.write(f"No lyrics found to {args.lyrics} by {args.artist}")
-            sys.exit(0)
+            return
         
-        sys.stdout.write(lyrics)
+        sys.stdout.write(lyrics+"\n\n")
 
         if args.save:
             save_to_folder(args.artist, args.lyrics, lyrics)
