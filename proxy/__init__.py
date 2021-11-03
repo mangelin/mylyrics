0c65728eb23fb5833f687660943c29403277ed75
Matteo Angelino <matteo.angelino@gmail.com>
2021-11-03 16:13:22 +0100

    Remove proxy facttory with a dict

--- proxy/__init__.py
+++ proxy/__init__.py
@@ -2,13 +2,7 @@ from .azlyrics import AzlyricsProxy
 from .elyrics import ElyricsProxy
 import config
 
-class ProxyLyricsFactory():
-    @staticmethod
-    def create_proxy(typ:str):
-        providers = config.providers()
-
-        if (typ.lower()) not in providers:
-            raise ValueError(f"Error: unkown provider {typ}\nvalid value are {','.join(providers)}")
-
-        targetclass = f"{typ.capitalize()}Proxy"
-        return globals()[targetclass]()
+ProxyLyricsFactory = {
+    config.AZLYRICS_PROXY.lower(): AzlyricsProxy(),
+    config.ELYRICS_PROXY.lower(): ElyricsProxy(),
+}
